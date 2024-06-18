c6aa37e6b80dff83986834a78dee8eb939d7cc08
felipegoraroficial <felipegoraroficial>
2024-06-18 19:14:39 +0000

    finalizando projeto

--- STEAM/gold/Gold_User.py
+++ STEAM/gold/Gold_User.py
@@ -1,5 +1,6 @@
 # Databricks notebook source
-from pyspark.sql.functions import when, col
+from pyspark.sql.functions import when, col, max, row_number
+from pyspark.sql.window import Window
 from pyspark.sql.types import StringType
 
 # COMMAND ----------
@@ -12,8 +13,33 @@ df.printSchema()
 
 # COMMAND ----------
 
+df.write.format("delta").mode("overwrite").save('dbfs:/gold/steam/user/steam_user')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE SCHEMA IF NOT EXISTS env_prod.steam
+
+# COMMAND ----------
+
+# Crar uma especificação de janela para particionar por appid e ordenar por file_data em ordem decrescente
+window_spec = Window.partitionBy("appid").orderBy(col("file_data").desc())
+
+# Adicionar uma coluna de número de linha para identificar as linhas mais recentes para cada appid
+df = df.withColumn("row_number", row_number().over(window_spec))
+
 display(df)
 
 # COMMAND ----------
 
-df.write.format("delta").mode("overwrite").save('dbfs:/gold/steam/user/steam_user')
+# Filtrar o dataframe para manter apenas as linhas com row_number = 1
+df = df.filter(col("row_number") == 1)
+
+# Eliminar a coluna row_number
+df = df.drop("row_number")
+
+display(df)
+
+# COMMAND ----------
+
+df.write.mode("overwrite").saveAsTable('steam.user')
